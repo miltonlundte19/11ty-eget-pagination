a61fa5c183d34469597753f0b91cc370ebd4a354
miltonlundte19 <milton.lund@elev.ga.ntig.se>
2021-10-04 11:09:30 +0200

    ✨ eleventy setings

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy('./src/css/');
+    eleventyConfig.addWatchTarget('./src/css/');
+
+    eleventyConfig.addPlugin(syntaxHighlight);
+    return {
+        dir: {
+            input: "src",
+            output: "dist"
+        },
+        passthroughFileCopy: true
+    };
+}
